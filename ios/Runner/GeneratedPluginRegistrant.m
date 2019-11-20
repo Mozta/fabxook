ee512b6560d58d3a720a2e40ad9bebdffaa48530
Rafael <viper_rafa_@hotmail.com>
2019-11-19 21:51:13 -0600

    Upload project

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_bluetooth_serial/FlutterBluetoothSerialPlugin.h>
+#import <qr_code_scanner/FlutterQrPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBluetoothSerialPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBluetoothSerialPlugin"]];
+  [FlutterQrPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterQrPlugin"]];
+}
+
+@end
